00a7feb6b38a56bf68a327e77dc16a0013ee5ca1
pricky-rck <pricky-rck>
2021-07-09 15:00:12 +0530

    First commit

--- BreakExample.java
+++ BreakExample.java
@@ -1,6 +1,6 @@
 //Java Program to demonstrate the use of break statement    
 //inside the for loop.  
-public class BreakExample {  
+reakExample {  
 public static void main(String[] args) {  
     //using for loop  
     for(int i=1;i<=10;i++){  
