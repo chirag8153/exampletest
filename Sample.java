e8ec6df1d8c6940c1f9a4ea95054ff4295549dfa
pricky-rck <pricky-rck>
2021-07-11 19:22:13 +0530

    To be added to master

--- Sample.java
+++ Sample.java
@@ -3,5 +3,9 @@
     public static void main(String args[]){  
 
      System.out.println("Hello Java");  
+
+     //Changes to be added to the origin master
+
+     System.out.println("Changes from branch to master origin");
     }  
 }  
